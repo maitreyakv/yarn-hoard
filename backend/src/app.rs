726f3694a8bce6cbe95a6330cc47a58d315a9737
Maitreya Venkataswamy <maitreyakv@gmail.com>
2025-03-16 20:36:01 -0400

    feat: adds user create endpoint skeleton

--- backend/src/app.rs
+++ backend/src/app.rs
@@ -1,10 +1,19 @@
-use axum::{Router, routing::get};
+use axum::{
+    Router,
+    routing::{get, post},
+};
 
-use crate::health_check::health_check;
+use crate::{health_check::health_check, users::create_user};
 
 /// Build the Axum application for the API.
 pub fn build_app() -> Router {
-    Router::new()
-        .route("/", get(|| async { "Hello, World!" }))
-        .route("/health", get(health_check))
+    Router::new().nest(
+        "/api",
+        Router::new().nest(
+            "/v1",
+            Router::new()
+                .route("/health", get(health_check))
+                .route("/users", post(create_user)),
+        ),
+    )
 }
--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,5 +1,6 @@
 /// This lib crate contains all the source code for the Yarn Hoard API.
 mod app;
 mod health_check;
+mod users;
 
 pub use app::build_app;
