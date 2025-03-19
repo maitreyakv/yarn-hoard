416725140aa81dcaa95d6f67538ad0d7d3bf8af0
Maitreya Venkataswamy <maitreyakv@gmail.com>
2025-03-18 22:19:09 -0400

    feat: adds users table to db

---
+++ backend/migration/src/m20250318_233214_create_users_table.rs
@@ -0,0 +1,36 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Users::Table)
+                    .col(pk_auto(Users::Id))
+                    .col(text(Users::Email))
+                    .col(string_len(Users::HashedPassword, 64))
+                    .col(string_len(Users::Salt, 8))
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Users::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum Users {
+    Table,
+    Id,
+    Email,
+    HashedPassword,
+    Salt,
+}
