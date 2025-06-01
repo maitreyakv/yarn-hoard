0bdbce49a4385f4e9349273bd25c71816e2eda58
Maitreya Venkataswamy <maitreyakv@gmail.com>
2025-05-31 22:07:24 -0400

    feat: more styling of signup page

--- src/lib/frontend/pages/signup.rs
+++ src/lib/frontend/pages/signup.rs
@@ -6,19 +6,21 @@ use crate::frontend::organisms::SignupForm;
 #[component]
 pub fn SignupPage() -> View {
     view! {
-        div(id="signup-container") {
-            Card {
-                CardHeader {
-                    CardTitle(text="Sign up for an account")
-                    "Enter your email and create a password"
-                }
-                CardContent {
-                    SignupForm()
-                }
-                CardFooter {
-                    div(class="text-center") {
-                        "Already a user? "
-                        a(href="/login") { "Login" }
+        div(id="signup-page") {
+            div(id="signup-container") {
+                Card {
+                    CardHeader {
+                        CardTitle(text="Sign up for an account")
+                        "Enter your email and create a password"
+                    }
+                    CardContent {
+                        SignupForm()
+                    }
+                    CardFooter {
+                        div(class="text-center") {
+                            "Already a user? "
+                            a(href="/login") { "Login" }
+                        }
                     }
                 }
             }
